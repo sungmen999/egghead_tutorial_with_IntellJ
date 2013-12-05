7e93beea3df1038f4f26e3a9d38405893bdb3a8c
sungmen999 <nanthasan999@gmail.com>
2013-12-05 21:31:06 +0700

    add files and fixed javascript

--- tutorial6/js/module.js
+++ tutorial6/js/module.js
@@ -1,7 +1,1 @@
 var myapps = angular.module('tutorial6',[]);
-
-myapps.filter('reverse', function(){
-	return function(text){
-		return text.split("").reverse().join("");
-	}
-});
---
+++ tutorial6/js/test/tutorial6ControllerTest.js
@@ -0,0 +1,5 @@
+describe('filter', function(){
+    beforeEach(
+        module('tutorial6')
+    );
+})
