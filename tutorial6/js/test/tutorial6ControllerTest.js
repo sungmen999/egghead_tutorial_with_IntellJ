01a92fb46bf013422b10806ecef443d6dab2f7e1
sungmen999 <nanthasan999@gmail.com>
2013-12-08 19:50:20 +0700

    added plus

--- tutorial6/js/test/tutorial6ControllerTest.js
+++ tutorial6/js/test/tutorial6ControllerTest.js
@@ -2,8 +2,8 @@
 
 describe('MainCtrl', function(){
     //we'll use this scope in our tests
-    var scope;
-    var controller;
+    var scope = null;
+    var ctrl = null;
 
     //mock Application to allow us to inject our own dependencies
     beforeEach(
@@ -19,8 +19,14 @@ describe('MainCtrl', function(){
         scope = $rootScope.$new();
 
         //declare the controller and inject our empty scope
-        controller = $controller('tutorial6Controller', {$scope: scope});
+        //injecting any services or other injectables we need.
+        ctrl = $controller('tutorial6Controller', {
+            $scope: scope
+            // it can mock service that you inject in controller
+            // *someService: mockService
+        });
     }));
+
     // tests start here
     describe('$scope.message', function(){
         it('should show "Hello World!" ', function(){
@@ -31,6 +37,26 @@ describe('MainCtrl', function(){
     describe('$scope.text', function(){
         it('should show "This is application service." ', function(){
             expect(scope.textService).toBe('This is application service.');
+        });
+    });
+
+    describe('ctrl.full_name', function(){
+         it('should show "Watcharapong Nanthasan"', function(){
+             expect(ctrl.full_name).toBe('Watcharapong Nanthasan');
+         });
+    });
+
+    describe('plus', function(){
+        it('should called', function(){
+            ctrl.plus = jasmine.createSpy('ctrl.plus');
+            ctrl.plus();
+            expect(ctrl.plus).toHaveBeenCalled();
+        });
+
+        it('should called with params', function(){
+            ctrl.plus = jasmine.createSpy('ctrl.plus');
+            ctrl.plus(1,2);
+            expect(ctrl.plus).toHaveBeenCalledWith(1,2);
         });
     });
 
--- tutorial6/js/tutorial6Controller.js
+++ tutorial6/js/tutorial6Controller.js
@@ -1,7 +1,13 @@
 'use strict';
 
-app.controller('tutorial6Controller', function($scope, mySharedService, notify) {
+app.controller('tutorial6Controller', function($scope, mySharedService) {
     var ctrl = this
+    ctrl.full_name = 'Watcharapong Nanthasan';
     $scope.message = 'Hello World!';
     $scope.textService = mySharedService.text;
+
+
+    ctrl.plus = function(numA, numB){
+        return (numA + numB);
+    };
 });
