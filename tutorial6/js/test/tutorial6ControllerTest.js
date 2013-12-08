ffd47f390b5e590fe1fc3fdd959495a143812dd0
sungmen999 <nanthasan999@gmail.com>
2013-12-08 20:16:19 +0700

    complete tutorial6

--- tutorial6/js/test/tutorial6ControllerTest.js
+++ tutorial6/js/test/tutorial6ControllerTest.js
@@ -47,16 +47,49 @@ describe('MainCtrl', function(){
     });
 
     describe('plus', function(){
-        it('should called', function(){
+
+        beforeEach(function() {
             ctrl.plus = jasmine.createSpy('ctrl.plus');
+        });
+
+        it('should called', function(){
             ctrl.plus();
             expect(ctrl.plus).toHaveBeenCalled();
         });
 
         it('should called with params', function(){
-            ctrl.plus = jasmine.createSpy('ctrl.plus');
             ctrl.plus(1,2);
             expect(ctrl.plus).toHaveBeenCalledWith(1,2);
+        });
+    });
+
+    describe("Multiple spies, when created manually", function() {
+        var tape;
+
+        beforeEach(function() {
+            tape = jasmine.createSpyObj('tape', ['play', 'pause', 'stop', 'rewind']);
+
+            tape.play();
+            tape.pause();
+            tape.rewind(0);
+        });
+
+        it("creates spies for each requested function", function() {
+            expect(tape.play).toBeDefined();
+            expect(tape.pause).toBeDefined();
+            expect(tape.stop).toBeDefined();
+            expect(tape.rewind).toBeDefined();
+        });
+
+        it("tracks that the spies were called", function() {
+            expect(tape.play).toHaveBeenCalled();
+            expect(tape.pause).toHaveBeenCalled();
+            expect(tape.rewind).toHaveBeenCalled();
+            expect(tape.stop).not.toHaveBeenCalled();
+        });
+
+        it("tracks all the arguments of its calls", function() {
+            expect(tape.rewind).toHaveBeenCalledWith(0);
         });
     });
 
